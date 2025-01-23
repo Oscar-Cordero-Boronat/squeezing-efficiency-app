30cf8b5ae3a8002722619008d732970cbafb8d27
Oscar Cordero Boronat <105495781+Oscar-Cordero-Boronat@users.noreply.github.com>
2025-01-23 13:00:01 +0100

    Update squeezing_fit.py

--- squeezing_fit.py
+++ squeezing_fit.py
@@ -228,8 +228,8 @@ Decay rate and detection frequency can also be considered according to the theor
 """)
 
 # Display the equation using LaTeX
-st.latex(r"SQ = 1 - \eta\cdot\frac{4\sqrt{P/P_{th}}}{\bigg(1 + \sqrt{\frac{P}{P_{th}}}\bigg)^2 + \bigg(\frac{f}{f_{HWHM}}\bigg)^2}")
-st.latex(r"ASQ = 1 + \eta\cdot\frac{4\sqrt{P/P_{th}}}{\bigg(1 - \sqrt{\frac{P}{P_{th}}}\bigg)^2 + \bigg(\frac{f}{f_{HWHM}}\bigg)^2}")
+st.latex(r"SQ = 1 - \eta\cdot\frac{4\sqrt{\frac{P}{P_{th}}}}{\bigg(1 + \sqrt{\frac{P}{P_{th}}}\bigg)^2 + \bigg(\frac{f}{f_{HWHM}}\bigg)^2}")
+st.latex(r"ASQ = 1 + \eta\cdot\frac{4\sqrt{\frac{P}{P_{th}}}{\bigg(1 - \sqrt{\frac{P}{P_{th}}}\bigg)^2 + \bigg(\frac{f}{f_{HWHM}}\bigg)^2}")
 st.latex(r"VAR(SQ) = 10 \cdot \log_{10}\Big(SQ \cdot \cos(\varepsilon)^2 + ASQ \cdot \sin(\varepsilon)^2\Big)")
 st.latex(r"VAR(ASQ) = 10 \cdot \log_{10}\Big(ASQ \cdot \cos(\varepsilon)^2 + SQ \cdot \sin(\varepsilon)^2\Big)")
 
